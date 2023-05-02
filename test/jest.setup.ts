cadface6e0b3ecb9b6b6fa5d6aac161b78c4d743
hugomoreira <hugo.moreira@pointr.co>
2023-05-02 15:37:42 -0300

    fix: http service moved folder

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,5 +1,5 @@
 import { httpMock } from "./mocks/http.mock";
 
-jest.mock("../services/http/http.service", () => ({
+jest.mock("../lib/services/http/http.service", () => ({
   Http: jest.fn().mockImplementation(() => httpMock)
 }))
