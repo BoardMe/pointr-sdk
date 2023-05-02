364a1d3e06e67f8176b5ce75ad6a045d94c41b5f
hugomoreira <hugo.moreira@pointr.co>
2023-05-02 11:08:50 -0300

    feature: add http service

---
+++ services/http/contract.ts
@@ -0,0 +1,44 @@
+import { AxiosError, AxiosRequestConfig } from "axios";
+import { Failure, Result, Success } from "./http.types";
+
+export abstract class Contract {
+  protected async try<T>(
+    fn: () => Promise<{ data?: T; status: number }>
+  ): Result<Success<T>, Failure> {
+    try {
+      const apiResponse = await fn();
+
+      if (apiResponse.status >= 400) {
+        throw apiResponse.data;
+      }
+
+      const isAxios = Boolean(apiResponse.data);
+
+      if (isAxios) {
+        return { data: apiResponse.data as T, error: null };
+      }
+
+      return { data: apiResponse as unknown as T, error: null };
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+
+      return {
+        error: {
+          message: (error.response?.data as any)?.message || error.response,
+          status: error.response?.status ?? 500,
+        },
+      };
+    }
+  }
+
+  abstract get<T>(
+    path: string,
+    config?: AxiosRequestConfig<any> | undefined
+  ): Result<Success<T>, Failure>;
+
+  abstract post<T>(path: string, data: any): Result<Success<T>, Failure>;
+
+  abstract put<T>(path: string, data: any): Result<Success<T>, Failure>;
+
+  abstract delete<T>(path: string): Result<Success<T>, Failure>;
+}
---
+++ services/http/http.service.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import { Contract } from "./contract";
+import {config} from "../../lib/config";
+
+export class Http extends Contract {
+  private client: AxiosInstance
+
+  constructor(private readonly apiKey: string) {
+    super()
+
+    this.client = axios.create({
+      baseURL: config.BASE_URL + "/api",
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      },
+    })
+  }
+
+  public async get<T>(path: string, config?: AxiosRequestConfig<any> | undefined) {
+    return this.try<T>(() => this.client.get(path, config))
+  }
+
+  public async post<T>(path: string, data?: any) {
+    return this.try<T>(() => this.client.post(path, data))
+  }
+
+  public async put<T>(path: string, data?: any) {
+    return this.try<T>(() => this.client.put(path, data))
+  }
+
+  public async delete<T>(path: string) {
+    return this.try<T>(() => this.client.delete(path))
+  }
+}
